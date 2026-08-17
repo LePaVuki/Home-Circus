4a554e3d9b25044af68f8a98da88c7cf3cdd4c6f
PaVuki <pavuki2000@gmail.com>
2026-08-17 08:34:33 +0500

    refactor: streamline logging functions to use log_message for consistency

--- scripts/utils/logging.sh
+++ scripts/utils/logging.sh
@@ -8,37 +8,51 @@ if [[ "${BASH_SOURCE}" == "${0}" ]]; then
 fi
 
 # Terminal colors
-RED='\033[1;31m'
-GREEN='\033[1;32m'
-BLUE='\033[1;34m'
-CYAN='\033[1;36m'
-YELLOW='\033[1;33m'
-ORANGE='\033[1;38;5;214m'
-NC='\033[0m' # No Color
+RED=$'\033[1;31m'
+GREEN=$'\033[1;32m'
+BLUE=$'\033[1;34m'
+CYAN=$'\033[1;36m'
+YELLOW=$'\033[1;33m'
+ORANGE=$'\033[1;38;5;214m'
+NC=$'\033[0m' # No Color
+
+log_message() {
+    local level="$1"
+    local message="$2"
+    local timestamp=$(date "+%Y-%m-%d %H:%M:%S.%3N")
+    echo "$timestamp | [$level] | $message"
+}
 
 log_info() {
-    echo -e "[${BLUE}INFO${NC}] $1"
+    local type="${BLUE}INFO${NC}"
+    log_message "$type" "$1"
 }
 
 log_success() {
-    echo -e "[${GREEN}SUCCESS${NC}] $1"
+    local type="${GREEN}SUCCESS${NC}"
+    log_message "$type" "$1"
 }
 
 log_skip() {
-    echo -e "[${YELLOW}SKIP${NC}] $1"
+    local type="${YELLOW}SKIP${NC}"
+    log_message "$type" "$1"
 }
 
 log_error() {
     # >&2 redirects the error message to standard error (stderr) instead of stdout
-    echo -e "[${RED}ERROR${NC}] $1" >&2
+    local type="${RED}ERROR${NC}"
+    log_message "$type" "$1" >&2
 }
 
 log_warning() {
-    echo -e "[${ORANGE}WARNING${NC}] $1"
+    local type="${ORANGE}WARNING${NC}"
+    log_message "$type" "$1"
 }
 
 log_debug() {
     if [ "$DEBUG_MODE" = true ]; then
-        echo -e "[${CYAN}DEBUG${NC}] $1"
+        local type="${CYAN}DEBUG${NC}"
+        executor=$(basename $0)
+        log_message "$type" "$executor | $1"
     fi
 }
